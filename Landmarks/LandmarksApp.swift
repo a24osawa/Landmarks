ff1b9d3bf6f200ab2f937dd3a9563462606f3ae9
Atsushi Osawa <a24osawa@gmail.com>
2022-06-11 22:42:20 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Atsushi Osawa on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
